231968f57677acc6d881e6c24735d1d72304ffce
BonesGrocerys <gleb-ilin@list.ru>
2023-10-09 14:31:39 +0300

    1

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	 import Header from './Header.svelte';
+	export let name;
+</script>
+
+<main>
+	<Header />
+	<h1>Hello {name}!</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		/* padding: 1em; */
+		max-width: 240px;
+		/* margin: 0 auto; */
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 768px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Header.svelte
@@ -0,0 +1,53 @@
+
+<header>
+        <div>
+            logo
+        </div>
+        <div class="button-content">
+            <button>Home</button>
+            <button>About</button>
+            <button>Login</button>
+        </div>
+</header>
+<style>
+     header {
+  background-color: #333;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
+.button-content{
+    display: flex;
+}
+.button-content > button {
+  margin-right: 10px;
+  border-radius: 15px;
+  width: 100px;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+    opacity: 50%;
+}
+
+
+@media screen and (max-width: 768px) {
+    .button-content {
+        flex-direction: column;
+        align-items: center;
+  }
+}
+@media screen and (max-width: 768px) {
+    header {
+    flex-direction: column;
+    align-items: flex-start;
+    align-items: center;
+  }
+}
+
+
+</style>
+ 
